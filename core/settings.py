eb85a8a31c46b185036affefe8a580749d956a20
onkarrrrrrr <onkarpathrikar2004@gmail.com>
2026-04-05 00:07:00 +0530

    vercel

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%*d^3c!_^2e)j(x108hme_f6xoas45-!now)p*1w-p&1w_-o2'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-u%*d^3c!_^2e)j(x108hme_f6xoas45-!now)p*1w-p&1w_-o2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
