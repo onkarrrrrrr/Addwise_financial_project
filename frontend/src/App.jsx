235e1547bd93290527191545856e4ac1835dbb58
onkarrrrrrr <onkarpathrikar2004@gmail.com>
2026-05-17 01:34:06 +0530

    ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Services from './components/Services';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Hero />
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <main>
+        <Hero />
+        <Services />
+      </main>
     </div>
   );
 }
